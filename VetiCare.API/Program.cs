a697e3d029bd3fa263f00b97111ea30153dd7968
valentinaalzate <vaev39@gmail.com>
2026-05-25 17:30:45 -0500

    Added Repositories, Services and FluentValidation in Program

--- VetiCare.API/Program.cs
+++ VetiCare.API/Program.cs
@@ -1,5 +1,11 @@
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using VetiCare.API.Validators;
 using VetiCare.DataAccess.Context;
+using VetiCare.DataAccess.Repositories;
+using VetiCare.Domain.Interfaces.Repositories;
+using VetiCare.Domain.Interfaces.Services;
+using VetiCare.Domain.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +14,22 @@ builder.Services.AddDbContext<VetiCareDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Repositories
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IMedicalRecordRepository, MedicalRecordRepository>();
+builder.Services.AddScoped<IMedicineRepository, MedicineRepository>();
+builder.Services.AddScoped<IPrescriptionRepository, PrescriptionRepository>();
+
+// Services
+builder.Services.AddScoped<IMedicalRecordService, MedicalRecordService>();
+builder.Services.AddScoped<IMedicineService, MedicineService>();
+builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
+
 // AutoMapper
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+// FluentValidation
+builder.Services.AddValidatorsFromAssemblyContaining<MedicalRecordValidator>();
 
 // Controllers + Swagger
 builder.Services.AddControllers();
