17f5527df49a0efc496461f2a120e8606b471381
donaldojbm7 <donaldojbm7@gmail.com>
2026-05-27 16:14:54 -0500

    feat(vet): add input validations to API endpoints

---
+++ VetiCare.API/Validators/VetValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using VetiCare.API.DTOs.Request;
+
+namespace VetiCare.API.Validators
+{
+    public class VetValidator : AbstractValidator<VetRequestDTO>
+    {
+        public VetValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("El nombre es obligatorio.")
+                .MaximumLength(100).WithMessage("El nombre no puede superar 100 caracteres.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("El apellido es obligatorio.")
+                .MaximumLength(100).WithMessage("El apellido no puede superar 100 caracteres.");
+
+            RuleFor(x => x.LicenseNumber)
+                .NotEmpty().WithMessage("El número de licencia es obligatorio.")
+                .MaximumLength(50).WithMessage("El número de licencia no puede superar 50 caracteres.");
+
+            RuleFor(x => x.Specialty)
+                .NotEmpty().WithMessage("La especialidad es obligatoria.")
+                .MaximumLength(150).WithMessage("La especialidad no puede superar 150 caracteres.");
+        }
+    }
+}
